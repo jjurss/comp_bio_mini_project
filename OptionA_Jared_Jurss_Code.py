59f8ca7f21072b5a69cf89c3e3777f6e8e360fc2
jjurss <47798142+jjurss@users.noreply.github.com>
2019-02-26 14:57:46 -0600

    Final Testing Complete
    Final debugging and minor output format changes.  Tested: working.

--- OptionA_Jared_Jurss_Code.py
+++ OptionA_Jared_Jurss_Code.py
@@ -15,8 +15,8 @@ fastq_file = "SRR8185310.fastq"
 ## The values for -k are seeds for the alogorithm
 ## The value for -t is the number of threads the assembler can use, can be changed based upon system specifications (more = faster)
 ## -o is the output directory for the results of the assembly
-SPAdes_command = "spades -k 55,77,99,127 -t 2 --only-assembler -s " + fastq_file + " -o SRR8185310_Assembly/"
-log.write(SPAdes_command)
+SPAdes_command = "spades -k 55,77,99,127 -t 6 --only-assembler -s " + fastq_file + " -o SRR8185310_Assembly/"
+log.write(SPAdes_command + " \n")
 os.system(SPAdes_command)
 ## This is the handle for the assembled contig file
 contig_handle = "SRR8185310_Assembly/contigs.fasta"
@@ -40,7 +40,7 @@ log.write("There are " + str(assembly_length) + " bp in the assembly. \n")
 ## --genus is the database being used by Prokka to annotate the assembly
 ## --mincontiglen is the minimum length for a contig to be considered
 prokka_commmand = "prokka --outdir prokka_results --prefix Ecoli --genus Escherichia --mincontiglen 1000 SRR8185310_Assembly/contigs.fasta"
-log.write(prokka_commmand)
+log.write(prokka_commmand + " \n")
 os.system(prokka_commmand)
 prokka_results = open("prokka_results/Ecoli.txt").readlines()
 ## Writes the results of the Prokka run as text in the log file
@@ -63,7 +63,7 @@ os.mkdir("TopHat_files")
 os.chdir("TopHat_files")
 ## Downloads the complete annotated genome of Escherichia coli
 os.system("wget ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_refseq/Bacteria/Escherichia_coli_K_12_substr__MG1655_uid57779/NC_000913.fna")
-## Downloads the reads for the E. coli transcriptome project of a K-12 derivative (BW38028) 
+## Downloads the reads for the E. coli transcriptome project of a K-12 derivative (BW38028)
 os.system("wget ftp://ftp.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR141/SRR1411276/SRR1411276.sra")
 ## Builds a BowTie2 index from the complete genome for use with TopHat2
 os.system("bowtie2-build NC_000913.fna NC_000913")
@@ -72,12 +72,12 @@ os.system("fastq-dump -I --split-files SRR1411276.sra")
 ## Calls TopHat2 to perform the mapping
 ## -o is the output directory for the TopHat run
 ## -p is the number of threads being used for the mapping, can be changed based on system specifications
-os.system("tophat2 -o SRR1411276_output --no-novel-juncs -p 2 NC_000913 SRR1411276_1.fastq")
+os.system("tophat2 -o SRR1411276_output --no-novel-juncs -p 6 NC_000913 SRR1411276_1.fastq")
 os.chdir("SRR1411276_output")
 ## Calls Cufflinks
 ## -o is the output directory
 ## -p is the number of threads being used, can be changed based on system specifications
-os.system("cufflinks -o cufflinks_output -p 2 accepted_hits.bam")
+os.system("cufflinks -o cufflinks_output -p 6 accepted_hits.bam")
 ## Opens the results of the Cufflinks run for parsing
 data = open("cufflinks_output/transcripts.gtf").readlines()
 os.chdir("..")
@@ -85,13 +85,13 @@ os.chdir("..")
 output = open("Option1.fpkm", "w")
 ## Iterates through the results of the Cufflinks run
 ## Writes the desired data (seqname, start, end, strand, and FPKM value) to Option1.fpkm
-## This is a CSV file where each row is a gene 
+## This is a CSV file where each row is a gene
 for line in data:
     line_data = line.split("\t")
     output.write(line_data[0] + ", " + line_data[3] + ", " + line_data[4] + ", " + line_data[6] + ", ")
     attributes = line_data[8].split("; ")
     for attribute in attributes:
         if "FPKM" in attribute:
-            output.write(attribute + "\n")
+            output.write(attribute + " \n")
 log.close()
 output.close()
