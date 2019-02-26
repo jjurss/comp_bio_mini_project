9f7b27fce00447599a9256e52108dc18663643cd
jjurss <47798142+jjurss@users.noreply.github.com>
2019-02-26 00:28:42 -0600

    Update OptionA_Jared_Jurss_Code.py

--- OptionA_Jared_Jurss_Code.py
+++ OptionA_Jared_Jurss_Code.py
@@ -24,10 +24,10 @@ assembly_length = 0
 for contig in significant_contigs:
     assembly_length += len(contig)
 log.write("There are " + str(assembly_length) + " bp in the assembly. \n")
-prokka_commmand = "prokka --outdir prokka_results --prefix Ecoli --genus Escherichia --mincontiglen 1000 --quiet contigs.fasta"
+prokka_commmand = "prokka --outdir prokka_results --prefix Ecoli --genus Escherichia --mincontiglen 1000 --quiet SRR1815310_Assembly/contigs.fasta"
 log.write(prokka_commmand)
 os.system(prokka_commmand)
-prokka_results = open("OptionA_Jared_Jurss/SRR8185310_Assembly/prokka_results/--Ecoli.txt").readlines()
+prokka_results = open("OptionA_Jared_Jurss/prokka_results/--Ecoli.txt").readlines()
 for line in prokka_results:
     log.write(line)
 CDS = int(prokka_results[4].strip().split(" ")[1])
@@ -40,6 +40,8 @@ if tRNA > 89:
     log.write(" and " + str(tRNA - 89) + " additional tRNA")
 else:
     log.write(" and " + str(89 - tRNA) + " fewer tRNA")
+os.mkdir("TopHat_files")
+os.chdir("TopHat_files")
 os.system("wget ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_refseq/Bacteria/Escherichia_coli_K_12_substr__MG1655_uid57779/NC_000913.fna")
 os.system("wget ftp://ftp.ncbi.nhl.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR141/SRR1411276/SRR1411276.sra")
 os.system("bowtie2-build NC_000913.fna NC_000913")
@@ -47,8 +49,9 @@ os.system("fastq-dump -I --split-files SRR1411276.sra")
 os.system("tophat2 -o SRR1411276_output --no-novel-juncs NC_000913 SRR1411276_1.fastq")
 os.system("cd SRR1411276_output")
 os.system("cufflinks accepted_hits.bam")
-data = open("transcripts.gtf").readlines()
-output = open("OptionA_Jared_Jurss/Option1.fpkm", "w")
+data = open("TopHat_Files/SRR1411276_output/transcripts.gtf").readlines()
+os.chdir("..")
+output = open("Option1.fpkm", "w")
 for line in data:
     line_data = line.split("\t")
     output.write(line_data[0] + ", " + line_data[3] + ", " + line_data[4] + ", " + line_data[6] + ", ")
